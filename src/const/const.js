b0332fb987c209c6b8df6b201c3962b07b33f098
Ericka Heredia <80068890+Ericka-25@users.noreply.github.com>
2025-01-24 13:21:25 -0400

    const.js

--- src/const/const.js
+++ src/const/const.js
@@ -1,2 +1,2 @@
-export const baseUrl = "http://localhost:8080/api";
-//export const baseUrl = "https://diagrammerdb-nodejs-production.up.railway.app/api";
+//export const baseUrl = "http://localhost:8080/api";
+export const baseUrl = "https://backend-diagramador-d4ef17f66638.herokuapp.com/";
